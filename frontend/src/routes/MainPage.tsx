b97a88af4599d0030e1878b63d144b1663a84b30
lunaru100 <lunaruteyvat@gmail.com>
2025-09-18 11:11:48 +0200

    quick fix

--- frontend/src/routes/MainPage.tsx
+++ frontend/src/routes/MainPage.tsx
@@ -1,10 +1,12 @@
 import "./MainPage.css";
-import bg from "../assets/rui_main_page_bg.png";
 import Search from "../Components/Search";
 
 function MainPage() {
   return (
-    <div id="mainPage" style={{ backgroundImage: `url(${bg})` }}>
+    <div
+      id="mainPage"
+      style={{ backgroundImage: `url(/rui_main_page_bg.png)` }}
+    >
       <span id="slogan">HAVE AN ACE UP YOUR SLEEVE</span>
       <Search />
     </div>
